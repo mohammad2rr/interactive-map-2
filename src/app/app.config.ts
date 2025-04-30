b8e6c4ae47a44f963ead8c217c338ed7f39448ba
mmd2r <mohammad2rghdd@gmail.com>
2025-04-30 11:36:14 +0330

    added configs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
+import { mapReducer } from './store/map/map.reducer';
+import { MapEffects } from './store/map/map.effects';
+import { MapService } from './core/services/map.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    MapService,
+    provideStore({ map: mapReducer }),
+    provideEffects([MapEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+  ],
 };
