ea6cf77404841acccd054553d938993cbb267d00
mmd2r <mohammad2rghdd@gmail.com>
2025-05-05 16:28:11 +0330

    added img process mudule4

--- src/app/features/image-to-geoJson/components/editor/editor.component.ts
+++ src/app/features/image-to-geoJson/components/editor/editor.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ThemeService } from '@core/services/theme.service';
+import { ThemeService } from '../../../../core/services/theme.service';
 
 @Component({
   selector: 'app-editor',
